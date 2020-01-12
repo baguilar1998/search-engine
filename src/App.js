2aa498d12441c94ac36f66325f09bb15d126698d
Brian Kenji Aguilar <anjacular@gmail.com>
2020-01-12 15:13:03 -0500

    setting up intial page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import fakenews from './assets/fakenews.JPG';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App-ImageHeader">
+          <img src={fakenews} />
+        </div>
+        <div className="App-Body">
+          <SearchBar />
+        </div>
     </div>
   );
 }
